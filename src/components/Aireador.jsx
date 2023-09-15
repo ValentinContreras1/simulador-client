70de7ad40aeb95ea8de63fefd52cf7ad0442d8cd
oKidd <imnotokidd@gmail.com>
2023-09-15 02:01:27 -0300

    Perilla Aireador

--- src/components/Aireador.jsx
+++ src/components/Aireador.jsx
@@ -5,16 +5,18 @@ export const Aireador = ({}) => {
   const [frecuencia, setFrecuencia] = useState("")
 
   return (
-    <li className='fs-5 text-start'>
-      Aireador: {frecuencia}{" "}
+    <div className="row w-100 ps-3">
+      <li className=' fs-5 text-start col'>
+        Aireador: {frecuencia}{" "}
+      </li>
       <Knob
+        className="col-2 text-end"
         value={frecuencia}
         onChange={(e) => setFrecuencia(e.value)}
-        min={0}
-        max={100}
-        step={1}
-        textColor={'white'}
+        valueColor={"white"}
+        rangeColor={"#313131"}
+        size={30}
       />
-    </li>
+    </div>
   )
 }
