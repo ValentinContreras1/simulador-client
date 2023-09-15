86bdbdcbbf0ee24a19630b308e7f7f6b9521bd1d
oKidd <imnotokidd@gmail.com>
2023-09-15 02:33:31 -0300

    progress-bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
-export const Navbar = ({children, consumo}) => {
+import React, { useRef, useEffect, useState } from 'react';
+
+export const Navbar = ({children, consumo=20}) => {
+  
   return (
     <nav className="container">
       <div className="navbar bg-dark mt-4 ps-4 pe-4 rounded-4">
-        <div className="col-6 fs-4 text-white">Consumo: {consumo}</div>
+        <div className="col-6">
+          <div className="col-2 float-start fs-4 text-white text-end">Consumo:</div>
+          <div class="progress col-9 m-2 float-end" role="progressbar" aria-label="Example with label" aria-valuenow={consumo} aria-valuemin="0" aria-valuemax="100">
+            <div className="progress-bar" style={{ width: `${consumo}%` }}>{consumo}%</div>
+          </div>
+        </div>
         <div className="col-6 text-end">
           {children}
         </div>
