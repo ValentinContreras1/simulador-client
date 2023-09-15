9bfd245391e468d060093965d6cd522470c956d3
Ricardo Fernandez <rckfr.fr@hotmail.com>
2023-09-15 01:19:06 -0300

    knob

--- src/components/Aireador.jsx
+++ src/components/Aireador.jsx
@@ -1,5 +1,20 @@
-export const Aireador = ({children, frecuencia}) => {
+import { Knob } from "primereact/knob"
+import { useState } from "react"
+
+export const Aireador = ({}) => {
+  const [frecuencia, setFrecuencia] = useState("")
+
   return (
-    <li className='fs-5 text-start'>Aireador: {frecuencia}</li>
-  );
-};
+    <li className='fs-5 text-start'>
+      Aireador: {frecuencia}{" "}
+      <Knob
+        value={frecuencia}
+        onChange={(e) => setFrecuencia(e.value)}
+        min={0}
+        max={100}
+        step={1}
+        textColor={'white'}
+      />
+    </li>
+  )
+}
